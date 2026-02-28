7fc58f12576acd9013cf519b0dff8ead603f0fab
Venu Chakravorty <6874518+venuchakravorty@users.noreply.github.com>
2026-02-28 15:58:45 +0530

    Update start_vm.sh

--- start_vm.sh
+++ start_vm.sh
@@ -1,6 +1,33 @@
+install busybox.x86_64
+cd ~/work
+mkdir rootfs
+cd rootfs
+mkdir -p bin sbin etc proc sys dev usr/bin usr/sbin
+cp /usr/bin/busybox ./bin/
+cd bin
+for i in $(./busybox --list); do     ln -s busybox $i; done
+cd ..
+vim init
+
+# Contents of init:
+#!/bin/sh
+
+mount -t proc none /proc
+mount -t sysfs none /sys
+mount -t devtmpfs none /dev
+
+echo "Booted minimal system"
+exec /bin/sh
+# content ends
+
+chmod +x init
+cd ..
+mkdir initramfs
+cd rootfs
+find . | cpio -o --format=newc > ../initramfs/initramfs.cpio
+
 qemu-system-x86_64 \
--kernel ~/work/linux/arch/x86_64/boot/bzImage \
--initrd ~/initramfs-6.18.9-200.fc43.x86_64.img \
--append "root=/dev/sda3 rootflags=subvol=root rw console=ttyS0 nokaslr" \
--drive file=~/work/vms/fedora.qcow2,format=qcow2 \
--m 2G -nographic -s -S
+    -kernel ~/work/linux/arch/x86/boot/bzImage \
+    -initrd ~/work/initramfs/initramfs.cpio \
+    -append "console=ttyS0 nokaslr" \
+    -nographic -s -S
